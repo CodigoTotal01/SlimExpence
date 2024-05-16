03822f5a1fd1790d1c34e77e8c93c3af61a78c82
Christian Palacios Tarrillo <palaciostarrillochristian@gmail.com>
2024-05-16 00:18:39 -0500

    Finalizacion primera parte

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -3,47 +3,68 @@ import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:slimeexpence/screens/home/views/main_screen.dart';
+import 'package:slimeexpence/screens/stats/stats.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int index = 0;
+  late Color selectedItem = Colors.blue;
+  Color unselectedItem = Colors.grey;
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        //appBar: AppBar(),
-        bottomNavigationBar: ClipRRect(
-          borderRadius: const BorderRadius.vertical(
-            top: Radius.circular(30),
-          ),
-          child: BottomNavigationBar(
-              backgroundColor: Colors.white,
-              showSelectedLabels: false,
-              showUnselectedLabels: false,
-              elevation: 3,
-              items: const [
-                BottomNavigationBarItem(
-                    icon: Icon(CupertinoIcons.home), label: 'Home'),
-                BottomNavigationBarItem(
-                    icon: Icon(CupertinoIcons.graph_square_fill),
-                    label: 'Stats'),
-              ]),
+      //appBar: AppBar(),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.vertical(
+          top: Radius.circular(30),
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          shape: const CircleBorder(),
-          child: Container(
-              width: 60,
-              height: 60,
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  gradient: LinearGradient(colors: [
-                    Theme.of(context).colorScheme.tertiary,
-                    Theme.of(context).colorScheme.secondary,
-                    Theme.of(context).colorScheme.primary,
-                  ], transform: const GradientRotation(pi / 4))),
-              child: const Icon(CupertinoIcons.add)),
-        ),
-    body: MainScreen(),);
+        child: BottomNavigationBar(
+            onTap: (value) {
+              setState(() {
+                index = value;
+              });
+            },
+            selectedItemColor: Colors.red,
+            backgroundColor: Colors.white,
+            showUnselectedLabels: false,
+            showSelectedLabels: false,
+            elevation: 3,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.home,
+                      color: index == 0 ? selectedItem : unselectedItem),
+                  label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.graph_square_fill,
+                      color: index == 1 ? selectedItem : unselectedItem),
+                  label: 'Stats')
+            ]),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        shape: const CircleBorder(),
+        child: Container(
+            width: 60,
+            height: 60,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                gradient: LinearGradient(colors: [
+                  Theme.of(context).colorScheme.tertiary,
+                  Theme.of(context).colorScheme.secondary,
+                  Theme.of(context).colorScheme.primary,
+                ], transform: const GradientRotation(pi / 4))),
+            child: const Icon(CupertinoIcons.add)),
+      ),
+      body: index == 0 ? const MainScreen() : const StatScreen(),
+    );
   }
 }
